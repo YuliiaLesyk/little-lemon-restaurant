47a2d8fb335bbda8e301f2abcc3677797d1eb62b
Yuliia Lesyk <julija.fedurko@gmail.com>
2024-01-02 13:24:07 -0500

    Addind App.css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import Header from './Header';
-import Nav from './Nav';
-import Main from './Main';
-import Footer from './Footer';
 import './App.css';
+import Nav from './components/Nav';
+
+
 
 function App() {
   return (
-    <>
-    <Header />
-      <Nav />
-      <Main />
-      <Footer />
-    </>
+   <> 
+   <Nav/>
+   </>
   );
 }
+
+
 
 export default App;
