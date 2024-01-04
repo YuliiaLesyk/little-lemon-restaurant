79bc54d90462429f1d382d80405e6fb32e0ee126
Yuliia Lesyk <julija.fedurko@gmail.com>
2024-01-04 11:39:27 -0500

    created Menu.js

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import bannerImg from '../images/restauranfood.jpg'
+
+const Header = () => {
+  return (
+    <header className='header'>
+     <section>
+
+       {/*baner text*/}
+       <div className='banner'>
+        <h2>Little Lemon</h2>
+        <h3>Chicago</h3>
+        <p>The finest culinary creations with a touch of lemony goodness. Our chefs are dedicated to bringing you the best flavors with fresh, high-quality ingredients.</p>
+       <button aria-label='On Click'>Reserve Table</button>
+       </div>
+
+       {/*banner img*/}
+       <div className='banner-img'>
+        <img src={bannerImg} alt=''/>
+       </div>
+
+     </section>
+    </header>
+  );
+}
+
+export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../images/Logo.svg'
 
 
 const Nav = () => {
+    const[menuOpen, setMenuOpen] = useState(false);
+
+
+    const toggleMenu =() => {
+        setMenuOpen(!menuOpen);
+    }
   return (
-    <nav>
-     <a href=''>
+    <nav className={`navbar ${menuOpen? "open" : ""}`}>
+     <a href='/' className='logo'>
         <img src={logo} alt='logo'/>
      </a>
      {/* Mobile navbar*/}
-     <div>
+     <div className='menu-icon'onClick={toggleMenu}>
         <div className='bar'></div>
         <div className='bar'></div>
         <div className='bar'></div>
      </div>
 
       {/* Nav items*/}
-      <ul>
+      <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
         <li>
             <a href='/'>Home</a>
         </li>
